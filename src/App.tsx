ec99b094b087607f5dd9dda51da43e9b58a798b6
suspencer14 <spencer.delange@gmail.com>
2024-01-21 18:12:08 -0700

    important work

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import freddy from './assets/freddy.jpg'
+import spencer from '.assets/spencer.jpg'
 import './App.css'
 
 function App() {
@@ -9,25 +9,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://github.com/suspencer14" target="_blank">
+          <img src={spencer} className="logo" alt="Spencer" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/fzwirb/" target="_blank">
+          <img src={freddy} className="logo react" alt="Freddy" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((count) => count = Math.random() % 10)}>
+          {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }

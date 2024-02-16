8d9f07f7a459e9e2addd1cea6e2883157e2451dc
suspencer14 <spencer.delange@gmail.com>
2024-02-16 13:14:19 -0700

    Remove breadcrumbs, add chart pngs

--- src/App.tsx
+++ src/App.tsx
@@ -29,11 +29,11 @@ const App: React.FC = () => {
         />
       </Header>
       <Content style={{ padding: '0 48px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
+        {/* <Breadcrumb style={{ margin: '16px 0' }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
           <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
+        </Breadcrumb> */}
         <div
           style={{
             background: colorBgContainer,
